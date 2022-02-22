d7bb9ae8f4ea1dfb695d667ad805ead2f92825e6
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-02-22 18:07:12 +0100

    context REACT multiple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Layout from './Layout.js';
+
+// Context per al tema
+export const ThemeContext = React.createContext();
+ThemeContext.displayName = 'ContextTema';  
+
+// Context per a l'usuari loggeat
+export const UserContext = React.createContext();
+UserContext.displayName = 'ContextUsername';
+
+class App extends Component {
+
+  render() {
+
+      const {signedInUser, theme} = this.props;
+
+      return (
+        <ThemeContext.Provider value={theme}>
+          <UserContext.Provider value={signedInUser}>
+            <Layout />
+          </UserContext.Provider>
+        </ThemeContext.Provider>
+      );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App signedInUser="Pepe" theme="white"  />
   </React.StrictMode>,
   document.getElementById('root')
 );
